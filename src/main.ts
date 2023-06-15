5f6e419438b9201c75ec3747b7380ddd567b0781
Oleg Voitiuk <oleg.voitiuk.natife@gmail.com>
2023-06-15 19:22:37 +0300

    feat: config
    - added environment
    - added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = configService.getPort();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Genesis Beck End API')
+    .setDescription('Genesis Beck End API')
+    .setVersion('0.0.1')
+    .build();
+
+  app
+    .setGlobalPrefix('/v1/api')
+    .useGlobalPipes(new ValidationPipe(configService.getValidationOptions()));
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port, async () =>
+    console.log(`Server is running on: ${await app.getUrl()}`),
+  );
 }
 bootstrap();
